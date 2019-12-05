94b395c117d31387ed6bb77fee43ae667ca3661a
denis-l <0>
2019-12-05 10:31:10 +0500

    ADD counting stat

--- serial.js
+++ serial.js
@@ -3,44 +3,94 @@ const SerialPort = require('serialport');
 let timeStart;
 let time = 0;
 let count = 0;
-function exit() {
-  console.log("avg", time / count);
-  process.exit();
+let lost = 0;
+let lostTimeout = 0;
+let wrong = 0;
+
+let timeout;
+
+const SIZE = 200
+const TIMEOUT = 1000;
+
+const END_SYMBOL = "z";
+const BODY_SYMBOL = "a";
+
+const port1 = new SerialPort('/dev/ttyUSB0', {baudRate: 115200});
+const port0 = new SerialPort('/dev/ttyUSB1', {baudRate: 115200});
+
+
+port1.on('data', onData(port0));
+port1.on('error', function (err) {
+  console.error('Error1: ', err.message);
+});
+
+port0.on('data', onData(port1));
+port0.on('error', function (err) {
+  console.error('Error0: ', err.message);
+});
+
+function onData(port) {
+  let dataLen = 0;
+  return function onData(data) {
+    dataLen += data.length;
+    data = data.toString();
+    // console.log('Data1:', data);
+    if (data[data.length - 1] == END_SYMBOL) {
+      if (dataLen !== SIZE) {
+        console.log("dataLen", dataLen);
+        lost += Math.ceil(dataLen / SIZE);
+      }
+      dataLen = 0;
+      time += new Date() - timeStart;
+      count++;
+      write(port);
+    }
+    data.split("").forEach(function (char) {
+      if (char === BODY_SYMBOL || char === END_SYMBOL) {
+        return;
+      } else {
+        wrong++;
+      }
+    });
+  }
 }
 
-const port1 = new SerialPort('/dev/ttyUSB3', {baudRate: 115200});
-
-port1.on('data', function (data) {
-  // console.log('Data1:', data.toString());
-  if (data.toString().includes("z")) {
-    time += new Date() - timeStart;
-    count++;
-    send(port0);
-    setTimeout(exit, 1000);
-  }
-});
-port1.on('error', function (err) {
-  console.log('Error1: ', err.message);
-});
-
-
-const port0 = new SerialPort('/dev/ttyUSB2', {baudRate: 115200});
-port0.on('data', function (data) {
-  console.log('Data0:', data.toString());
-});
-port0.on('error', function (err) {
-  console.log('Error0: ', err.message);
-});
-
-function send(port) {
+function write(port) {
   timeStart = new Date();
-  port.write("x".repeat(10000) + 'z', function (err) {
+  port.write(BODY_SYMBOL.repeat(SIZE - 1) + END_SYMBOL, function (err) {
     if (err) {
-      return console.log('Error on write: ', err.message);
+      return console.error('Error on write: ', err.message);
     } else {
       // console.log('message written');
     }
   });
+  clearTimeout(timeout);
+  timeout = setTimeout(function () {
+    lostTimeout++;
+    write(port);
+  }, TIMEOUT);
+};
+
+write(port0);
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on("data", logResult);
+
+process.on("beforeExit", logResult2);
+process.on("SIGTERM", logResult2);
+process.on("SIGINT", logResult2);
+
+function logResult2() {
+  logResult();
+  process.exit();
 }
 
-send(port0);
+function logResult() {
+  console.log("avg:", time / count);
+  console.log("time:", time);
+  console.log("count:", count);
+  console.log("wrong:", wrong);
+  console.log("lost:", lost);
+  console.log("lostTimeout:", lostTimeout);
+}
